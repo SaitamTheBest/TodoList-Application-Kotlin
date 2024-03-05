cd4f76acd42b43915c8a5b84605a39476db0d212
Matias DEVEZE <matias.deveze@etu.unilim.fr>
2024-03-05 14:09:09 +0100

    delete basic interface

--- src/androidApp/src/main/java/com/example/todolistapplication/android/MainActivity.kt
+++ src/androidApp/src/main/java/com/example/todolistapplication/android/MainActivity.kt
@@ -13,28 +13,6 @@ import com.example.todolistapplication.Greeting
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    GreetingView(Greeting().greet())
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
+        setContentView(R.layout.layout)
     }
 }
